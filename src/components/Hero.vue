05bbaf962acd3631be9cde0c0b20c532db774e4b
alathereonn <lucifaa2@gmail.com>
2025-12-08 21:51:04 +0800

    feat: almost done

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -12,7 +12,7 @@
         <h1 class="text-5xl font-semibold tracking-tight text-balance text-pink-500 sm:text-7xl">Welcome to ECHOES: The Undying Follower WIKI</h1>
         <p class="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">ECHOES - The Undying Follower is a 2D-pixel horror game. This game is a top-down Role-Playing Game (RPG), where players will play as a police detective named Jason Blood who is conducting an investigation in a small town called Eden's Gloria which mysteriously disappeared from the map, but when the player arrives in Eden's Gloria, the player will be chased by a terrible entity called The Prophet who will always chase the player relentlessly until the player is caught. Besides The Prophet, there are also other entities that follow The Prophet called The Followers. The goal of this game is to find out the mystery that the player is working on regarding the town and this terrible entity. Discover everything about your favorite characters, lore, and gameplay mechanics.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a href="/plots" class="rounded-md bg-pink-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Learn More</a>
+          <a href="/plots" class="rounded-md bg-pink-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-800 hover:text-pink-500 border border-pink-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Learn More</a>
         </div>
       </div>
     </div>
